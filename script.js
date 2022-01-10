9f21a6f773d6ad1a5daf2507fe2bd91283dc7b2e
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-01-10 19:21:56 +0000

    created base javascript file

--- script.js
+++ script.js
@@ -1,30 +1,23 @@
 // words
-
 var words = ["italy", "france", "germany", "spain", "india", "pakistan", "dubai"];
 
 var characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var maxGuesses = 10;
-
-
 var wordArray = [ ];
-var guessesRemaining = 0;
 var winCount = 0;
 var lossCount = 0;
 var isFinished = false;
-var randomWord = document.getElementById("random-word");
+var guessedLetters = [ ];
+var chosenWord = [];
+var secondsLeft = 60;
+
+var timer = document.getElementById("time-count");
+
+document.getElementById("time-count").textContent = secondsLeft;
+
 var startGame = document.getElementById("start-btn");
 
-var guessedLetters = [ ];
-var guessedLettersElement = document.getElementById("letters-guessed")
-guessedLettersElement.textContent = guessedLetters;
-
-
 startGame.addEventListener("click", playGame)
-
-var chosenWord = [ ];
-
-var secondsLeft
 
 
 function playGame() {
@@ -36,20 +29,19 @@ function playGame() {
 
     // store word as "_" replacing amount of letters within word
 
-    var wordArray = [ ];
-
     for(var i = 0; i < chosenWord.length; i++) {
-        wordArray[i] = "_";
-        randomWord.textContent = wordArray;
+        wordArray[i] = " _ ";
     }
 
     // execute countdown function
     setCountdown();
+    updateDisplay();
 }
 
 
 
 // function begins countdown timer from 60 
+
 function setCountdown(){
     var secondsLeft = 60;
     var timeCount = document.getElementById("time-count")
@@ -61,55 +53,108 @@ function setCountdown(){
             secondsLeft --;
             timeCount.textContent = secondsLeft;
         }
-        if (secondsLeft === 0){
+        if (secondsLeft <= 0){
             timeCount.style.color = "red";
+        
+            // lostGame = true;
+            
         }
 
     }, 1000);
 }
 
 
-// event listner to check for key press event
 
+
+// event listener to check for key press event
 
 document.addEventListener('keyup', function(event){
 
     var key = event.key.toLowerCase()
-    
+
+    if (isFinished) {
+        playAgain()
+        isFinished = false;
+    }
 
     if (characters.includes(key)) {
-        checkKey(key)
+        checkKey(key);
+        youWin();
+        youLose();
+        updateDisplay();
     }
+
     
-})
+});
 
 
+function playAgain() {
+
+    var playAgain = confirm("Do you want to play again?");
+
+    if (playAgain) {
+        playGame()
+    };
+
+};
 
 
 // Checks whether key pressed responds to letter within word
+
 function checkKey(letter) {
+
 
     if (guessedLetters.indexOf(letter) === -1) {
         guessedLetters.push(letter);
-        guessedLettersElement.textContent = guessedLetters;
+        updateDisplay()
     }
+
     if (chosenWord.indexOf(letter) === -1) {
-        console.log("letter not in word")
+        // play an incorrect sound
     }
+
     else {
-        console.log("letter is in word")
         for (var i = 0; i < chosenWord.length; i++) {
+
             if (letter === chosenWord[i]) {
                 wordArray[i] = letter;
-                randomWord.textContent = wordArray;
+                console.log(wordArray);
+                updateDisplay()
             }
-
         }
+    }
+};
+
+
+
+function updateDisplay() {
+
+    document.getElementById("random-word").textContent = wordArray.join("");
+    document.getElementById("time-count").textContent = secondsLeft;
+    document.getElementById("letters-guessed").textContent = guessedLetters;
+    document.getElementById("win-count").textContent = winCount;
+
+};
+
+
+
+function youWin() {
+    if (wordArray.indexOf(" _ ") === -1){
+        console.log("you win")
+        winCount++;
+        isFinished = true;
+        updateDisplay()
+    }
+};
+
+
+function youLose() {
+    if (timer.textContent == 0){
+        console.log("you lose")
+        lossCount++;
+        isFinished = true;
+        updateDisplay()
     }
 }
 
-
-
-
-
-
+updateDisplay()
